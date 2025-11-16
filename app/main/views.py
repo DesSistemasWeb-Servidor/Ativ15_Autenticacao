f55658804b08f75907b37ba1ee4f9b1162d53cd5
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-11-16 14:30:41 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from . import main
-from app.models import User
+from app.models import User, Role
 
 @main.route('/')
 def index():
     usuarios = user.query.all()
-    return render_template('index.html', usuarios=usuarios, user=User)
+    return render_template('index.html', usuarios=usuarios, user=User, role=Role)
