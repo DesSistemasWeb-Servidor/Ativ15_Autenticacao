ce824d5be8dfd4cdd9636dc5bf40fe2d56ef8d56
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-11-16 14:57:58 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -4,5 +4,8 @@ from app.models import User, Role
 
 @main.route('/')
 def index():
-    usuarios = User.query.all()
-    return render_template('index.html', usuarios=usuarios, user=User, role=Role)
+    if not current_user.is_anonymous:
+        usuarios = User.query.all()
+        return render_template('index.html', usuarios=usuarios, user=User, role=Role)
+    else:
+        return render_template('index.html')
